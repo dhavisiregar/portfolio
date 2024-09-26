99675726438e14340c080c3ca6a5869511d54808
Sultan Muhammad Dhavi <dhavisrg@Sultans-MacBook-Air.local>
2024-09-26 22:02:06 +0700

    make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import logo from "../assets/logo.png";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6 relative">
       <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="logo" className="w-10 mx-2" />
       </div>
-      <div>
+      <div className="hidden md:flex">
         <ul className="flex z-10 items-center gap-8 text-sm relative">
           <li>
             <a href="#about" className="hover:text-purple-700">
@@ -39,10 +48,69 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+      <div className="md:hidden flex items-center z-10 ml-auto">
+        <button onClick={toggleMenu} className="text-2xl focus:outline-none">
+          {isOpen ? "✖️" : "☰"} {/* Hamburger icon */}
+        </button>
+      </div>
+      <div
+        className={`absolute top-16 right-0 w-full bg-black shadow-lg md:hidden ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <ul className="flex flex-col items-center gap-4 py-4">
+          <li>
+            <a
+              href="#about"
+              className="hover:text-purple-700"
+              onClick={toggleMenu}
+            >
+              About Me
+            </a>
+          </li>
+          <li>
+            <a
+              href="#technologies"
+              className="hover:text-purple-700"
+              onClick={toggleMenu}
+            >
+              Technologies
+            </a>
+          </li>
+          <li>
+            <a
+              href="#experience"
+              className="hover:text-purple-700"
+              onClick={toggleMenu}
+            >
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#projects"
+              className="hover:text-purple-700"
+              onClick={toggleMenu}
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className="hover:text-purple-700"
+              onClick={toggleMenu}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
           href="https://www.linkedin.com/in/sultan-muhammad-dhavi/"
           target="_blank"
+          rel="noopener noreferrer"
           className="z-10 hover:text-purple-700"
         >
           <FaLinkedin />
@@ -50,6 +118,7 @@ const Navbar = () => {
         <a
           href="https://github.com/dhavisiregar"
           target="_blank"
+          rel="noopener noreferrer"
           className="z-10 hover:text-purple-700"
         >
           <FaGithub />
@@ -57,6 +126,7 @@ const Navbar = () => {
         <a
           href="https://www.instagram.com/dhavisiregar/"
           target="_blank"
+          rel="noopener noreferrer"
           className="z-10 hover:text-purple-700"
         >
           <FaInstagram />
@@ -64,9 +134,10 @@ const Navbar = () => {
         <a
           href="https://x.com/siregardhavi"
           target="_blank"
+          rel="noopener noreferrer"
           className="z-10 hover:text-purple-700"
         >
-          <FaSquareXTwitter />
+          <FaTwitterSquare />
         </a>
       </div>
     </nav>
